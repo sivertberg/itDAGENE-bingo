b8d01a35c0291f528aa55ae14db302fddd47cbd6
Magnus Schjølberg <magnus.schjolberg@gmail.com>
2019-02-03 23:42:42 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Tinder-bingo-web/precache-manifest.007d93f47e4d3b93ee01838f69b87844.js"
+  "/Tinder-bingo-web/precache-manifest.2ec0afcdfe734f086bf13c5a4d14a813.js"
 );
 
 workbox.clientsClaim();
